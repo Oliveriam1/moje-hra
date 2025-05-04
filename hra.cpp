4710edbb6492f6a7fd6c0330774e7e7f4b520d39
Oliveriam <oliveriam1234@gmail.com>
2025-05-04 16:39:18 +0200

    Čtvrtá menší úprava

--- hra.cpp
+++ hra.cpp
@@ -6,7 +6,7 @@
 using namespace std::chrono;
 // Nezapomen poupravit statistiky u postav(vigor, ...)
 
-struct Hrac {
+struct Hrac{
     std::string jmeno;
     std::string role;
     int vigor; // Urcuje zivoty
@@ -16,26 +16,26 @@ struct Hrac {
     int penize;
 };
 
-struct Nepritel {
+struct Nepritel{
     std::string njmeno;
     int nvigor;
     int nstrength; //Jak velke poskozeni ti bude udelovat
 };
 
-struct Miniboss {
+struct Miniboss{
     std::string mbjmeno;
     int mbvigor;
     int mbstrength;
 };
 
-struct Boss {
+struct Boss{
     std::string bjmeno;
     int bvigor;
     int bstrength;
     int bspecials;
 };
 
-struct Vesnice {
+struct Vesnice{
     std::string obchod; //Pro koupeni potion ktery pridaji boost do zivotu, nebo inteligennce, ...
     std::string mlekarna; //Pro vyhealeni
     std::string mag; //Pro vylepsen
@@ -45,7 +45,7 @@ std::string randomtext(){
     std::string moznosti[6] = {
         "Skibdii boilet",
         "bobardinokrokodini",
-        "bombinigusini",
+        "bombombinigusini",
         "kapucinoassasino",
         "Tralelotralala",
         "zibrazubrazibralini"
@@ -59,7 +59,7 @@ std::string randomtext(){
 
 }
 
-void utok(){
+void utok(const std::string &njmeno, int &nstrength, int &vigor){
     std::string zadanytext = randomtext();
     std::cout << "Mate 5 sekund na napsani: " << zadanytext << std::endl;
     
@@ -71,15 +71,18 @@ void utok(){
     auto end = steady_clock::now();
     
     auto trvani = duration_cast<seconds>(end - start).count();
-
+    // udleat funkci na vybrani nepritele oliku nezapomen
     if (trvani <= 5 && vstupnitext == zadanytext) {
         std::cout << "Vyborne! Ubral si " << std::endl; //dodelat vypsani damage
     } else {
+        if (njmeno == "1"){
+            std::cout << "Ubral ti: " << vigor - nstrength << std::endl;
+        }
         std::cout << "Ubral ti -1 zivot (" << trvani << " sekund)" << std::endl; //dodelat vypsani emocionalniho poskozeni
     }
 }
 
-void overeni(std::string &potvrzeni, std::string role, Hrac &h) {
+void overeni(std::string &potvrzeni, std::string role, Hrac &h){
     bool platnost_potvrzeni = false;
     while (!platnost_potvrzeni){
         std::cout << "Jste si jisti ze chcete tuto roli? (Y/N): ";
@@ -133,7 +136,7 @@ void overeni(std::string &potvrzeni, std::string role, Hrac &h) {
     }
 }
 
-Nepritel createNepritel (const std::string &njmeno) {
+Nepritel createNepritel (const std::string &njmeno){
     Nepritel n;
     n.njmeno = njmeno;
         if (njmeno == "1"){ // PRIDAT JMENA
@@ -146,22 +149,21 @@ Nepritel createNepritel (const std::string &njmeno) {
         }
 }
 
-Miniboss createMiniboss (const std::string &mbjmeno) {
+Miniboss createMiniboss (const std::string &mbjmeno){
     Miniboss mb;
     mb.mbjmeno = mbjmeno;
 
 }
 
-Boss createBoss (const std::string &bjmeno) {
+Boss createBoss (const std::string &bjmeno){
     Boss b;
     b.bjmeno = bjmeno;
     b.bjmeno = "Thornhost";
     b.bvigor = 100;
     b.bspecials = -10;
-
 }
 
-Hrac createHrac(const std::string &jmeno, const std::string &role) {
+Hrac createHrac(const std::string &jmeno, const std::string &role){
     Hrac h;
     std::string potvrzeni;
     h.jmeno = jmeno;
@@ -194,16 +196,46 @@ Hrac createHrac(const std::string &jmeno, const std::string &role) {
     return h;
 }
 
-void zacatek(std::string &jmeno, std::string &role) {
+Vesnice createVesnice(const std::string &obchod, const std::string &mlekarna, std::string &mag){
+    Vesnice v;
+        char vyberobchodu;
+    
+
+        std::cout << "Něco olivere dopiššš"; // dopsat uvitani do vesnice
+        std::cout << "Vyber si kam chces zajit: [obchod, mlekarna, mag] ";
+        std::cin >> vyberobchodu;
+
+        switch(vyberobchodu){
+                case 'obchod':
+                //udelat vic funkci obchodu, kde bude vzdycky jiny vyber veci
+                break;
+                case 'mlekarna':
+                std::cout << "Co to bude?";
+                std::cout << "   Heal potion (+3)" << std::endl;
+                break;
+                case 'mag':
+                //Funkci Na vylepseni statistik
+                break;
+        }
+}
+
+void cara(){
+    for (int i = 0; i < 113; i++){
+        std::cout << "_";
+    }
+}
+
+void zacatek(std::string &jmeno, std::string &role){
     std::cout << "Vitej v me hre, dobrodruhu. Pred tebou se oteviraji dvere do sveta plneho nebezpeci, tajemstvi a dobrodruzstvi. Jako hrdina mas pred sebou spoustu vyzev." << std::endl << "Tvoje cesta zacina nyni, ale nez se vydas do boje, je treba pochopit, system utoku." << std::endl << "Boj bude probihat formou psani textu. Zadny obycejny boj s mecem nebo ciste random zaletisost to nebude." << std::endl << "Misto toho tedy budes muset rychle reagovat na vyzvy, ktere ti budou predkladany ve forme textu. Cely system bude zalezet na tom, jak rychle dokazes napsat odpoved." << std::endl << "Statistiky tve postavy ovlivni tvuj uspech v boji:" << std::endl;
     std::cout << std::endl;
     std::cout << "Vigor urcuje, jak dlouho vydrzis v boji." << std::endl;
     std::cout << "Endurance urcuje, casovy limit na psani odpovedi." << std::endl;
     std::cout << "Intelligence ovlivnuje slozitost textu, ktery budes muset napsat." << std::endl;
     std::cout << "Strength urcuje, velikost textu." << std::endl;
-        for (int i = 0; i < 113; i++){
-            std::cout << "_";
-        }
+        cara();
+    std::cout << "\n \nV prubehu hry budes narazet na mista zvane vesnice," << std::endl;
+    std::cout << "kde si budes moct vylepsovat statistiky, lecit se ci nakupovat itemy..." << std::endl;
+        cara();
     std::cout << std::endl << std::endl;
     std::cout << "Jak se budes jmenovat?: ";
     std::cin >> jmeno;
@@ -211,13 +243,14 @@ void zacatek(std::string &jmeno, std::string &role) {
     std::cin >> role;
 }
 
-
-main() {
+int main(){
     std::string njmeno;
+    int nstrength;
 
     std::string jmeno;
     std::string role;
     std::string potvrzeni;
+    int vigor;
 
     zacatek(jmeno, role);
 
@@ -225,7 +258,7 @@ main() {
 
     overeni(potvrzeni, role, hrac);
 
-    utok();
+    utok(njmeno, nstrength, vigor);
     std::cout << "Zivoty postavy (" << hrac.jmeno << "): " << hrac.vigor << std::endl; //Oli to je treba na vypsani zivotu
 
 
